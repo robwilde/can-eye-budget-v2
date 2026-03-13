3234cef9ec82fa0a5eac97e945e59da3b65bec02
mrwilde <robert@mrwilde.com>
2026-03-13 11:12:13 +1000

    feat: add route for Smoke Test view
    - Registers a new route `smoke-test` for easier access to the Smoke Test view.
    - Aligns with recent updates to support testing and debugging of app functionality.

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::view('smoke-test', 'smoke-test')->name('smoke-test');
 });
 
 require __DIR__.'/settings.php';
